f495819d341f74876e73c8a61e9ffaf6e470bc31
Shunichi Shinohara <shino@basho.com>
2014-07-17 12:28:08 +0900

    Fix unit test bug, type bug and suppress dializer warnings

--- src/riak_cs_multibag.hrl
+++ src/riak_cs_multibag.hrl
@@ -5,7 +5,7 @@
 -define(WEIGHT_KEY,    <<"weight">>).
 
 -record(weight_info, {
-          bag_id :: riak_cs_multibag:bag_id(),
+          bag_id :: bag_id(),
           weight :: non_neg_integer(),
           opts = [] :: proplists:proplist()   %% Not used
           }).
